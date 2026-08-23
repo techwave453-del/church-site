c0f7f0f175bd86516f0c975fdf0312d06215c6af
techwave453-del <techwave453@gmail.com>
2026-08-23 20:02:16 +0300

    Wire services UI to extracted module

--- admin/admin-services.js
+++ admin/admin-services.js
@@ -1,3 +1,5 @@
 (function(){
-  window.adminServiceHelpers={add:function(){if(!window.adminSiteData)return;window.adminSiteData.services.push({title:'',time:'',image:''});if(window.renderAdminEditor)window.renderAdminEditor('services')},remove:function(i){if(!window.adminSiteData)return;if(!confirm('Remove this service?'))return;window.adminSiteData.services.splice(i,1);if(window.renderAdminEditor)window.renderAdminEditor('services')}};
+  function render(){if(window.renderAdminEditor)window.renderAdminEditor('services')}
+  window.adminServiceHelpers={add:function(){if(!window.adminSiteData)return;window.adminSiteData.services.push({title:'',time:'',image:''});render()},remove:function(i){if(!window.adminSiteData)return;if(!confirm('Remove this service?'))return;window.adminSiteData.services.splice(i,1);render()}};
+  window.addService=function(){window.adminServiceHelpers.add()};
 })();
